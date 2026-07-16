037b13bb51b95d3f47f67d1cd8355faeae19a50b
Ojas <ojassat12@gmail.com>
2026-07-16 11:33:56 +1000

    Add working Xcode project

---
+++ Welleva/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Welleva
+//
+//  Created by Ojas Satarkar on 16/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Welleva/WellevaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellevaApp.swift
+//  Welleva
+//
+//  Created by Ojas Satarkar on 16/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct WellevaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

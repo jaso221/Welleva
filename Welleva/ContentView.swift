822d55e9e644120ecb24e42152d7463e7f62d2c0
sichenwang-sally <Sichen.wang@student.uts.edu.au>
2026-07-20 13:55:33 +1000

    contentview(can check login)

--- Welleva/ContentView.swift
+++ Welleva/ContentView.swift
@@ -1,12 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Binding private var currentPage : Int
+    
     var body: some View {
-        LearningView()
+        ZStack{
+            switch currentPage{
+            case 0: WelcomePage(currentPage: $currentPage)
+            case 1: IntroView(currentPage: $currentPage)
+            case 2: Intro1(currentPage: $currentPage)
+            case 3: Intro2(currentPage: $currentPage)
+            case 4: EncryptedView(currentPage: $currentPage)
+            case 5: PrivacyFirst(currentPage: $currentPage)
+            case 6: Login(currentPage: $currentPage)
+            case 7: SignUp(currentPage: $currentPage)
+                
+            default: WelcomePage(currentPage: $currentPage)
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    WelcomePage(currentPage: .constant(0))
 }
 
